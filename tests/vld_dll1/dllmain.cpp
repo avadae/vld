d99f721b0cb236eeabc439b370bc01376e7802f6
grrava <alex.vandenabeele@gmail.com>
2024-11-27 19:27:35 +0100

    Updated tests to vs 2022

---
+++ tests/vld_dll1/dllmain.cpp
@@ -0,0 +1,33 @@
+#include "pch.h"
+#include <stdlib.h>
+#include <assert.h>
+
+#include <string>
+
+// Here we static initialize a string within the DLL
+// Previous versions of VLD would flag this as a leak, make sure it does not
+static std::string my_string("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH: {
+        void* leak = malloc(9);
+        break;
+    }
+    case DLL_THREAD_ATTACH: {
+        break;
+    }
+    case DLL_THREAD_DETACH: {
+        break;
+    }
+    case DLL_PROCESS_DETACH: {
+        //assert(1 == VLDGetThreadLeaksCount(GetCurrentThreadId()));
+        break;
+    }
+    }
+    return TRUE;
+}
